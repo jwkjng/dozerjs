873cca3d0691ff11502fafae3731edb2f8f82395
fluidbyte <kent@fluidbyte.net>
2014-02-07 01:55:59 +0000

    Added express-slash to handle trailing slashes on dynamic routes

--- internals/index.js
+++ internals/index.js
@@ -2,6 +2,7 @@
 // It initializes the api and web controllers and starts the static web service
 var express = require('express');
 var app = express();
+var slash = require('express-slash');
 var multipart = require('connect-multiparty');
 var multiparty = multipart();
 var config = require('../config.js');
@@ -10,6 +11,8 @@ var web = require('./web.js');
 
 // Basic express config
 app.enable('strict routing');
+app.use(app.router);
+app.use(slash());
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -17,7 +20,7 @@ app.use(express.urlencoded());
 api.init();
 
 // Process API calls
-app.all('/api/:endpoint/*', multiparty, api.process);
+app.all('/api/:endpoint*', multiparty, api.process);
 
 // Serve static assets
 app.get('/*', web.serve);
