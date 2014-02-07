65a3cef31eb5a8f6248d9c8170b648b13814fe23
fluidbyte <kent@fluidbyte.net>
2014-02-07 01:44:19 +0000

    Adding multipart and form data, param handling

--- internals/index.js
+++ internals/index.js
@@ -2,15 +2,21 @@
 // It initializes the api and web controllers and starts the static web service
 var express = require('express');
 var app = express();
+var multipart = require('connect-multiparty');
+var multiparty = multipart();
 var config = require('../config.js');
 var api = require('./api.js');
 var web = require('./web.js');
+
+// Basic express config
+app.use(express.json());
+app.use(express.urlencoded());
 
 // Initialize API
 api.init();
 
 // Process API calls
-app.all('/api/:endpoint/*', api.process);
+app.all('/api/:endpoint/*', multiparty, api.process);
 
 // Serve static assets
 app.get('/*', web.serve);
