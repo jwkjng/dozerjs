304f13e3510519add38a662e142a66b656513158
fluidbyte <kent@fluidbyte.net>
2014-02-09 22:59:21 +0000

    Just saving some stuff I am toying with for data model traversal

--- components/validation.js
+++ components/validation.js
@@ -1,7 +1,24 @@
 // Validation for data models
 var validation = function (data, model) {
 
+  function processNode(parent,key,value) {
+        if (parent !== "") {
+            console.log(parent + " >> " + key + ": "+value);
+        } else {
+            console.log(key + ": " + value);
+        }
+    }
 
+    function traverseNodes(parent, obj, fn) {
+        for (var i in obj) {
+            fn.apply(this,[parent, i,obj[i]]);
+            if (obj[i] !== null && typeof(obj[i])==='object' && Object.prototype.toString.call( obj[i] ) !== '[object Array]') {
+                traverseNodes(parent + " >> " + i,obj[i],fn);
+            }
+        }
+    }
+
+    traverseNodes("root", data, processNode);
 
 };
 
