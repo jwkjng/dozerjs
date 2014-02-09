71a73c9553f72a46c7fd9c1e81074c97faefe378
fluidbyte <kent@fluidbyte.net>
2014-02-09 19:13:10 +0000

    Additional mods to users model for validation

--- models/users.js
+++ models/users.js
@@ -1,11 +1,15 @@
 // Example "users" model
 module.exports = {
   '_id': 'uid',
-  'name': 'string',
+  'username': 'string',
   'email': 'email',
   'password': 'password',
   'active': 'boolean',
   'tags': 'array',
   'posts': 'number',
-  'joined': 'timestamp'
+  'joined': 'timestamp',
+  'bio': {
+    'fname': 'string',
+    'lname': 'string'
+  }
 };
