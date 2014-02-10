e6b6b67e1e9097588ff12130036a608ea06b3bb3
fluidbyte <kent@fluidbyte.net>
2014-02-10 01:04:06 +0000

    Validation now successfully lining up data to model

--- components/validation.js
+++ components/validation.js
@@ -1,24 +1,31 @@
 // Validation for data models
 var validation = function (data, model) {
 
-  function processNode(parent,key,value) {
-        if (parent !== "") {
-            console.log(parent + " >> " + key + ": "+value);
-        } else {
-            console.log(key + ": " + value);
-        }
-    }
+  function processNode(key,value,valid) {
+      console.log(key + ": " + value + ": " + valid)
+  }
 
-    function traverseNodes(parent, obj, fn) {
-        for (var i in obj) {
-            fn.apply(this,[parent, i,obj[i]]);
-            if (obj[i] !== null && typeof(obj[i])==='object' && Object.prototype.toString.call( obj[i] ) !== '[object Array]') {
-                traverseNodes(parent + " >> " + i,obj[i],fn);
-            }
-        }
-    }
+  function traverseNodes(obj, model, fn) {
+      for (var i in obj) {
+          if (model.hasOwnProperty(i)) {
+              if (obj[i] !== null && typeof(obj[i])==='object' && Object.prototype.toString.call( obj[i] ) !== '[object Array]') {
+                  traverseNodes(obj[i],model[i],fn);
+              } else {
+                  fn.apply(this,[i,obj[i],model[i]]);
+              }
+          } else {
+              console.log("Invalid property");
+              return false;
+          }
+      }
+      return true;
+  }
 
-    traverseNodes("root", data, processNode);
+  if(traverseNodes(data, model, processNode)) {
+      console.log("DONE");
+  } else {
+      console.log("FAILED");
+  }
 
 };
 
